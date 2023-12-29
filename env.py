626a19d3383c3c0478f08669c69a730138a3baaf
Mehatab Pathan <mehatab@Mehatabs-Air.station>
2023-12-29 15:18:18 +0000

    adding card payment for stripe

--- env.py
+++ env.py
@@ -1,6 +1,4 @@
 import os
 os.environ["STRIPE_PUBLIC_KEY"]="pk_test_51OJxlVH8Vqa8Gi3UoV1DMIexrYD9jgDlC5M06HWtBK1pPwKThpKq52zPxyolf0momp68IvijV24mF6gg66DPqcTr00awCKbOSK"
 os.environ["STRIPE_SECRET_KEY"]="sk_test_51OJxlVH8Vqa8Gi3UgKtiJWpsCAXf3P4cu92kuHx7ErKQCMRb3RYcqZLybgO1nNGIxmoopPeYYMdm8GTvqPJoVGml003ODHi07h"
-os.environ["STRIPE_WH_SECRET"]="whsec_PQ90fnTnHXSKtLZyt4coBiK0vbLLrRfR"
-os.environ["EMAIL_HOST_USER"]="mehatab.pathan231@gmail.com"
-os.environ["EMAIL_HOST_PASS"]="zpsn fnmc koet nvgq"
+
