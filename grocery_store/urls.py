19a741564f7f6fd390bf87586042761166acceba
Mehatab Pathan <mehatab@Mehatabs-MacBook-Air.local>
2024-01-04 19:14:52 +0000

    adding 500,404,400,403 pages code

--- grocery_store/urls.py
+++ grocery_store/urls.py
@@ -17,7 +17,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-from .views import handler404
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -29,5 +29,4 @@ urlpatterns = [
     path('profile/', include('profiles.urls')),
     path('contact/', include('contact.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-handler404 = 'grocery_store.views.handler404'
 
