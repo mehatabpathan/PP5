41bf36ec19eb72d751781633d98f137a9e79719c
Mehatab Pathan <mehatab@Mehatabs-Air.station>
2024-01-04 00:05:56 +0000

    sending real emails

--- env.py
+++ env.py
@@ -3,4 +3,4 @@ os.environ["STRIPE_PUBLIC_KEY"]="pk_test_51OJxlVH8Vqa8Gi3UoV1DMIexrYD9jgDlC5M06H
 os.environ["STRIPE_SECRET_KEY"]="sk_test_51OJxlVH8Vqa8Gi3UgKtiJWpsCAXf3P4cu92kuHx7ErKQCMRb3RYcqZLybgO1nNGIxmoopPeYYMdm8GTvqPJoVGml003ODHi07h"
 os.environ["STRIPE_WH_SECRET"]="whsec_yPlbCIQx1YO3nNmat9mVawHR1iBeqZDS"
 os.environ["EMAIL_HOST_USER"]="mehatab.pathan231@gmail.com"
-os.environ["EMAIL_HOST_PASS"]="zpsn fnmc koet nvgq"
+os.environ["EMAIL_HOST_PASS"]="symytioklgcrdiiw"
